cae543d687046c09984fe840d5eccd48bdbdd893
oleg d <olegd1c@gmail.com>
2016-12-13 15:52:32 +0200

    13122016

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 // создаём Express-приложение
 var app = express();
 
+app.use(express.logger());
+
 // создаём маршрут для главной страницы
 // http://localhost:8080/
 app.get('/', function(req, res) {
@@ -11,6 +13,10 @@ app.get('/', function(req, res) {
 });
 
 // запускаем сервер на порту 8080
-app.listen(8080);
+//app.listen(8080);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 // отправляем сообщение
 console.log('Сервер стартовал!'); 
